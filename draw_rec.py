85d497063626fd99e4812aa95313228d3d73412f
paparn <paparnball@gmail.com>
2021-02-15 22:27:08 +0700

    add img2pdf

--- draw_rec.py
+++ draw_rec.py
@@ -5,8 +5,8 @@ import cv2
 
 path = '/home/paparn/Downloads/raw'
 dest = '/home/paparn/Downloads/rec'
-x, y = 118, 118
-w, h = 2313, 3080
+x, y = 177, 236
+w, h = 2185, 2835
 
 for f_path in sorted(glob(path + '/*')):
     print(f_path)
@@ -18,3 +18,5 @@ for f_path in sorted(glob(path + '/*')):
     thickness = 10
     image = cv2.rectangle(image, start_point, end_point, color, thickness)
     cv2.imwrite(os.path.join(dest, file_name), image)
+
+os.system(f'img2pdf {dest}/* -o {dest}/flickers_qr.pdf')
